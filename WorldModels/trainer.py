d5791bfcf7e85f6e696d09dbfa11a97d9320a6b2
JussiKarkkainen <juskark@gmail.com>
2022-11-01 18:29:13 +0200

    add get_latents

--- WorldModels/trainer.py
+++ WorldModels/trainer.py
@@ -11,7 +11,7 @@ class VAETrainingState(NamedTuple):
     params: hk.Params
     opt_state: optax.OptState
 
-class VAETrainer:
+class VTrainer:
     def __init__(self, dataset, episodes):
         self.dataset = dataset 
         self.model = hk.without_apply_rng(hk.transform(self._forward))
@@ -81,28 +81,26 @@ class LSTMTrainingState(NamedTuple):
     params: hk.Params
     opt_state: optax.OptState
 
-class LSTMTrainer:
-    def __init__(self, dataset, vae_encode_batch, episodes, v_model_params, v_model):
+class MTrainer:
+    def __init__(self, dataset, episodes, v_model_params):
         self.dataset = dataset
-        # TODO: create latent vectors for MDM-RNN at runtime instead of here, this doesn't scale
-        self.vae_encode_batch = iter(jnp.array([v_model.apply(v_model_params, 
-            dataset.get_train_inputs(32, iterator=False)[i])[3] for i in range((episodes // 32) - 1)]))
-    
         
         self.episodes = episodes
-        self.v_model_params = v_model_params # Train M-model together with fixed params of V-model
-        self.v_model = v_model
         self.batch_size = 32
-        self.train_labels = dataset.get_targets(self.batch_size)
         self.train_inputs = dataset.get_train_inputs(self.batch_size)
+        self.train_targets = dataset.get_seq_targets(self.batch_size)
         self.train_actions = dataset.get_train_actions(self.batch_size)
         self.rng = jax.random.PRNGKey(seed=42)
-        self.lstm_model = hk.without_apply_rng(hk.transform(self._forward))
+        self.m_model = hk.without_apply_rng(hk.transform(self._forward))
         self.loss_fn = jax.jit(self._loss_fn)
         self.update_weights = jax.jit(self._update_weights)
         self.optimizer = optax.adam(1e-4)
         self.num_epochs = (episodes // self.batch_size) - 1
         self.LSTMState = LSTMTrainingState(params=None, opt_state=None)
+        
+        self.v_model_params = v_model_params # Train M-model together with fixed params of V-model
+        self.v_model = hk.without_apply_rng(hk.transform(self.v_forward)) 
+
         '''
         wandb.init(project="WorldModel LSTM")
         wandb.config = {
@@ -111,7 +109,12 @@ class LSTMTrainer:
             "batch_size": 32
         }
         '''
-    def _unroll_net(self, z, a):
+    def v_forward(self, x):
+        v_model = models.ConvVae()
+        z, mu, logsigma, decoded = v_model(x)
+        return z
+    
+    def _unroll_rnn(self, z, a):
         lstm_net = models.LSTM()
         lstm_initial_state = lstm_net.initial_state(32)
         state = lstm_net(z, a, lstm_initial_state)
@@ -131,9 +134,26 @@ class LSTMTrainer:
         return LSTMTrainingState(params=params, opt_state=opt_state)
 
     def make_initial_state(self, rng, z, action):
-        init_params = self.lstm_model.init(rng, z, action)
+        init_params = self.m_model.init(rng, z, action)
         opt_state = self.optimizer.init(init_params)
         return LSTMTrainingState(params=init_params, opt_state=opt_state)
+    
+    def get_latents(self, obs, targets):
+        ''' 
+        Turn observations to latents so they can be used by M-model
+        Obs.shape is (bs, seq_len, h, w, c) loop over batches and sequences to produce latents
+        '''
+        latents, target_latents = [], []
+        for i in range(obs.shape[0]):
+            o_batch, t_batch = [], []
+            for j in range(obs.shape[1]):
+                o_batch.append(self.v_model.apply(self.v_model_params, jnp.expand_dims(obs[i][j], axis=0)))
+                t_batch.append(self.v_model.apply(self.v_model_params, jnp.expand_dims(targets[i][j], axis=0)))
+            latents.append(jnp.array(o_batch))
+            target_latents.append(jnp.array(t_batch))
+        latents = jnp.array(latents)
+        target_latents = jnp.array(target_latents)
+        return latents, target_latents
 
     def step(self, x, a):
         # TODO: Input should be of shape (batch_size, seq_len, H, W, C) 
@@ -142,11 +162,11 @@ class LSTMTrainer:
         return loss
 
     def train(self):
-        dummy_z = next(self.vae_encode_batch)
-        dummy_a = next(self.train_actions)
+        actions = self.train_actions
+        latents, target_latents = self.get_latents(self.train_inputs, self.train_targets)
         self.LSTMState = self.make_initial_state(self.rng, dummy_z, dummy_a)
-        for x, a in zip(self.train_inputs, self.train_actions):
-            loss = self.step(x, a)
+        for z_t, z_t1, a in zip(latents, self.target_latents, actions):
+            loss = self.step(z_t, z_t1, a)
             #wandb.log(["loss": loss])
 
         return self.LSTMState, self.lstm_model
